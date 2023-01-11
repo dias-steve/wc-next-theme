45b511842b955f39ad19cb0c3ee30b83467cf24b
Steve Dias <stevediaspro@gmail.com>
2023-01-11 18:54:56 +0100

    [add] test

---
+++ utils/Product/cart/cart.utils.js
@@ -0,0 +1,15 @@
+export const addProductToCard = (product, productList) => {
+    return null
+}
+
+export const removeProductToCard = (product, productList) => {
+    return null
+}
+
+export const getTotalPrice = (productList) => {
+
+}
+
+export const getTotalQuatity = (productList) => {
+    
+}
